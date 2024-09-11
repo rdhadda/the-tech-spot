ef44b4dd1bc171fc4032faf6bdb131d7dd44b552
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-09-11 15:14:34 +0100

    feat: added pre-population to the form data if the user is authenticated

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404,
+    HttpResponse
+)
 from django.views.decorators.http import require_POST
-from .forms import OrderForm
+from .forms import OrderForm, Order
 from django.contrib import messages
 from django.conf import settings
 from bag.contexts import bag_contents
 import stripe
 import json
 from products.models import Product
-from .forms import OrderForm, Order
 from .models import OrderLineItem
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 
 @require_POST
@@ -26,6 +33,7 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -51,8 +59,8 @@ def checkout(request):
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.save()
-            for item_id, quantity in bag.items(): 
-                try:               
+            for item_id, quantity in bag.items():
+                try:
                     product = Product.objects.get(id=item_id)
                     if isinstance(quantity, int):
                         order_line_item = OrderLineItem(
@@ -63,7 +71,8 @@ def checkout(request):
                         order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
+                        "One of the products in your bag wasn't"
+                        "found in our database."
                         "Please call us for assistance!")
                     )
                     order.delete()
@@ -71,7 +80,8 @@ def checkout(request):
             
             # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -79,7 +89,8 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request, "There's nothing in your bag at"
+                           "the moment")
             return redirect(reverse('products'))
         
         current_bag = bag_contents(request)
@@ -87,11 +98,30 @@ def checkout(request):
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-        )       
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
 
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info
+        #  the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()        
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -105,7 +135,8 @@ def checkout(request):
         
     }
 
-    return render(request, template, context )
+    return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
@@ -113,6 +144,28 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
     Your order number is {order_number}. A confirmation \
     email will be sent to {order.email}.')
@@ -124,5 +177,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
-    
+
     return render(request, template, context)
