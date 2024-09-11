549c95ceb53043405027008a2959ec1d6ef96b1f
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-09-11 10:25:32 +0100

    feat: added profile urls

--- the_tech_spot/settings.py
+++ the_tech_spot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^a14t)+xg*bpkw1x36h-pbbot7djy_)c=e#@3du=-g7_&^@3y9'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
