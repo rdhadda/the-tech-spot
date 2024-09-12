e618a516b813efcd44d4c5c4b6fb3a0383beaa64
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-09-12 11:20:07 +0100

    feat: secured the views with login_required decorator

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.contrib.auth.decorators import login_required
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -57,6 +58,7 @@ def all_products(request):
     
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
     """ A view to show a products details     
     """
@@ -69,6 +71,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
@@ -94,6 +97,7 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
@@ -122,6 +126,7 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
