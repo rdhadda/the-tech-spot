362178548cddce58b36a1ce80ba21422f16553f5
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-09-16 14:18:37 +0100

    testing: added unit test for bag views

---
+++ bag/test_views.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.urls import reverse
+from products.models import Product
+
+
+class TestBagViews(TestCase):
+
+    def setUp(self):
+        # create a product
+        self.product = Product.objects.create(name='Test Product', price=100)
+
+    def test_get_bag_page(self):
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
+
+    def test_add_to_bag(self):
+
+        post_data = {
+            'quantity': 2,
+            'redirect_url': reverse('product_detail', args=[self.product.id])
+        }
+
+        response = self.client.post(reverse('add_to_bag', args=[self.product.id]), post_data)
+
+        bag = self.client.session.get('bag', {})
+
+        self.assertIn(str(self.product.id), bag)
+        self.assertEqual(bag[str(self.product.id)], 2)
+
+    def test_remove_from_bag(self):
+
+        post_data = {
+            'quantity': 1,
+            'redirect_url': reverse('product_detail', args=[self.product.id])
+        }
+
+        self.client.post(reverse('add_to_bag', args=[self.product.id]), post_data)
+
+        bag = self.client.session.get('bag', {})
+
+        response = self.client.post(reverse('remove_from_bag', args=[self.product.id]))
+
+        bag = self.client.session.get('bag', {})
+
+        self.assertNotIn(str(self.product.id), bag)
+
+        
+
+
+
+    
+
+
