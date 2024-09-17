42653cb7ea75919833574665c9c6a450966e6bd7
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-09-17 15:46:13 +0100

    feat: added contact form to admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'name',
+        'email',
+        'subject',
+        'message',
+        'created_at'
+    )
+
+    fields = (
+        'name',
+        'email',
+        'subject',
+        'message',
+        'created_at',
+        'resolved',  
+    )
+
+    list_display = (
+        'email',
+        'created_at',
+        'resolved'
+    )
+
+    ordering = ('-created_at',)
+
+
+admin.site.register(Contact, ContactAdmin)
+
